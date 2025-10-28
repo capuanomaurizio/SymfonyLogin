998add72725e238d43491a9da92ada9344e7812f
capuanomaurizio <mauricapuano@gmail.com>
2025-10-28 08:43:53 +0100

    Testing code

--- src/Controller/CollectionsController.php
+++ src/Controller/CollectionsController.php
@@ -4,27 +4,108 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Document\Collection;
+use App\Document\Process;
 use App\Service\DynamicUserDatabaseManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CollectionsController extends AbstractController
 {
 
     public function __construct(
-        private readonly DynamicUserDatabaseManager $dbManager
+        private readonly DynamicUserDatabaseManager $dbManager,
+        private readonly SerializerInterface $serializer
     ){}
 
-    #[Route('/collections')]
-    public function index(): Response
+    /**
+     * @throws ExceptionInterface
+     */
+    #[Route('/collections/processes')]
+    public function processesList(): Response
     {
-        $repo = $this->dbManager->getManagerForCurrentUser()->getRepository(Collection::class);
-        $coll = $repo->findAll();
-        dump($this->dbManager->getManagerForCurrentUser());
-        dump($coll);
-        exit;
+        $processes = $this->dbManager->getManagerForCurrentUser()->getRepository(Process::class)->findAll();
+        $json = $this->serializer->serialize($processes, 'json', ['groups' => ['process:read']]);
+        return $this->render('processes.html.twig', ['processes' => $json]);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    #[Route('/collections/process/{id}', name: 'process_route')]
+    public function processTree(string $id): Response
+    {
+
+        $processTree = $this->dbManager->getManagerForCurrentUser()->getRepository(Process::class)->getProcessTree(
+            $this->dbManager->getManagerForCurrentUser()->getRepository(Process::class)->findOneby(['id' => $id])
+        );
+        $json = $this->serializer->serialize($processTree, 'json', ['groups' => ['process:read']]);
+        return $this->render('process-tree.html.twig', ['processTree' => $json]);
+    }
+
+    #[Route('/collections/initialize')]
+    public function initialize(): Response
+    {
+        /*$comp1 = (new Component())->setName("componente1");
+
+        $comp11 = (new Component())->setName("componente11");
+        $comp12 = (new Component())->setName("componente12");
+        $comp13 = (new Component())->setName("componente13");
+        $comp14 = (new Component())->setName("componente14");
+
+        $comp111 = (new Component())->setName("componente111");
+        $comp112 = (new Component())->setName("componente112");
+
+        $comp131 = (new Component())->setName("componente131");
+        $comp132 = (new Component())->setName("componente132");
+        $comp133 = (new Component())->setName("componente133");
+
+        $comp141 = (new Component())->setName("componente141");
+
+        $comp1->addChildComponent($comp11);
+        $comp1->addChildComponent($comp12);
+        $comp1->addChildComponent($comp13);
+        $comp1->addChildComponent($comp14);
+        $comp11->setParentComponent($comp1);
+        $comp12->setParentComponent($comp1);
+        $comp13->setParentComponent($comp1);
+        $comp14->setParentComponent($comp1);
+
+        $comp11->addChildComponent($comp111);
+        $comp11->addChildComponent($comp112);
+        $comp111->setParentComponent($comp11);
+        $comp112->setParentComponent($comp11);
+
+        $comp13->addChildComponent($comp131);
+        $comp13->addChildComponent($comp132);
+        $comp13->addChildComponent($comp133);
+        $comp131->setParentComponent($comp13);
+        $comp132->setParentComponent($comp13);
+        $comp133->setParentComponent($comp13);
+
+        $comp14->addChildComponent($comp141);
+        $comp141->setParentComponent($comp14);
+
+
+        $proc = (new Process())->setName("primoProcesso");
+        $proc->setComponent($comp1);
+
+        $docManager = $this->dbManager->getManagerForCurrentUser();
+        $docManager->persist($comp141);
+        $docManager->persist($comp133);
+        $docManager->persist($comp132);
+        $docManager->persist($comp131);
+        $docManager->persist($comp112);
+        $docManager->persist($comp111);
+        $docManager->persist($comp14);
+        $docManager->persist($comp13);
+        $docManager->persist($comp12);
+        $docManager->persist($comp11);
+        $docManager->persist($comp1);
+        $docManager->persist($proc);
+        $docManager->flush();*/
 
         return $this->render('base.html.twig');
     }
